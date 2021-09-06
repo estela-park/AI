1826b0437376cde05f18a410c1c9aab4ad3eaf38
estela-park <stellapark401@naver.com>
2021-09-07 02:09:26 +0900

    greener

--- Keras_Modelling/keras50_matplotlib_Kor.py
+++
@@ -1,3 +0,0 @@
-# kor-font trouble shooting
-from matplotlib import rc
-rc('font', family='Malgun Gothic')
--- Keras_Modelling/keras55_1_save_npy.py
+++ Keras_Modelling/keras55_1_save_npy.py
@@ -6,63 +6,63 @@ datasets = load_iris()
 x_data_iris = datasets.data   
 y_data_iris = datasets.target 
 
-np.save('./_save/_npy/k55_x_data_iris.npy', arr=x_data_iris)
-np.save('./_save/_npy/k55_y_data_iris.npy', arr=y_data_iris)
+np.save('../_save/_npy/k55_x_data_iris.npy', arr=x_data_iris)
+np.save('../_save/_npy/k55_y_data_iris.npy', arr=y_data_iris)
 
 datasets = load_boston()
 x_data_boston = datasets.data 
 y_data_boston = datasets.target
 
-np.save('./_save/_npy/k55_x_data_boston.npy', arr=x_data_boston)
-np.save('./_save/_npy/k55_y_data_boston.npy', arr=y_data_boston)
+np.save('../_save/_npy/k55_x_data_boston.npy', arr=x_data_boston)
+np.save('../_save/_npy/k55_y_data_boston.npy', arr=y_data_boston)
 
 datasets = load_breast_cancer()
 x_data_cancer = datasets.data  
 y_data_cancer = datasets.target
 
-np.save('./_save/_npy/k55_x_data_cancer.npy', arr=x_data_cancer)
-np.save('./_save/_npy/k55_y_data_cancer.npy', arr=y_data_cancer)
+np.save('../_save/_npy/k55_x_data_cancer.npy', arr=x_data_cancer)
+np.save('../_save/_npy/k55_y_data_cancer.npy', arr=y_data_cancer)
 
 datasets = load_diabetes()
 x_data_diabetes = datasets.data   
 y_data_diabetes = datasets.target 
 
-np.save('./_save/_npy/k55_x_data_diabetes.npy', arr=x_data_diabetes)
-np.save('./_save/_npy/k55_y_data_diabetes.npy', arr=y_data_diabetes)
+np.save('../_save/_npy/k55_x_data_diabetes.npy', arr=x_data_diabetes)
+np.save('../_save/_npy/k55_y_data_diabetes.npy', arr=y_data_diabetes)
 
 datasets = load_wine()
 x_data_wine = datasets.data   
 y_data_wine = datasets.target 
 
-np.save('./_save/_npy/k55_x_data_wine.npy', arr=x_data_wine)
-np.save('./_save/_npy/k55_y_data_wine.npy', arr=y_data_wine)
+np.save('../_save/_npy/k55_x_data_wine.npy', arr=x_data_wine)
+np.save('../_save/_npy/k55_y_data_wine.npy', arr=y_data_wine)
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data() 
 # (60000, 28, 28) (60000,) (10000, 28, 28) (10000,)
+print(type(x_train))
+x_data_mnist = np.concatenate((x_train, x_test), axis=0)
+y_data_mnist = np.concatenate((y_train, y_test), axis=0)
 
-x_data_mnist = np.array(x_train, x_test)
-y_data_mnist = np.array(y_train, y_test)
-
-np.save('./_save/_npy/k55_x_data_mnist.npy', arr=x_data_mnist)
-np.save('./_save/_npy/k55_y_data_mnist.npy', arr=y_data_mnist)
+np.save('../_save/_npy/k55_x_data_mnist.npy', arr=x_data_mnist)
+np.save('../_save/_npy/k55_y_data_mnist.npy', arr=y_data_mnist)
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data() 
 # (50000, 32, 32, 3) (10000, 32, 32, 3)
 # (50000, 1) (10000, 1)
 
-x_data_cifar10 = np.array(x_train, x_test)
-y_data_cifar10 = np.array(y_train, y_test)
+x_data_cifar10 = np.concatenate((x_train, x_test), axis=0)
+y_data_cifar10 = np.concatenate((y_train, y_test), axis=0)
 
-np.save('./_save/_npy/k55_x_data_mnist.npy', arr=x_data_cifar10)
-np.save('./_save/_npy/k55_y_data_mnist.npy', arr=y_data_cifar10)
+np.save('../_save/_npy/k55_x_data_mnist.npy', arr=x_data_cifar10)
+np.save('../_save/_npy/k55_y_data_mnist.npy', arr=y_data_cifar10)
 
 (x_train, y_train), (x_test, y_test) = cifar100.load_data() 
 # (50000, 32, 32, 3) (10000, 32, 32, 3)
 # (50000, 1) (10000, 1)
 
-x_data_cifar100 = np.array(x_train, x_test)
-y_data_cifar100 = np.array(y_train, y_test)
+x_data_cifar100 = np.concatenate((x_train, x_test), axis=0)
+y_data_cifar100 = np.concatenate((y_train, y_test), axis=0)
 
-np.save('./_save/_npy/k55_x_data_mnist.npy', arr=x_data_cifar100)
-np.save('./_save/_npy/k55_y_data_mnist.npy', arr=y_data_cifar100)
+np.save('../_save/_npy/k55_x_data_mnist.npy', arr=x_data_cifar100)
+np.save('../_save/_npy/k55_y_data_mnist.npy', arr=y_data_cifar100)
