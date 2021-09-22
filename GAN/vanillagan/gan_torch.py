66b5917b7d8137f1d03c16179d1e180ddf36267b
estela-park <stellapark401@naver.com>
2021-09-22 20:31:55 +0900

    gan

--- GAN/vanillagan/gan_torch.py
+++ GAN/vanillagan/gan_torch.py
@@ -242,3 +242,72 @@ def get_gen_loss(gen, disc, criterion, num_images, z_dim, device):
     return gen_loss
 
 
+cur_step = 0
+mean_generator_loss = 0
+mean_discriminator_loss = 0
+test_generator = True 
+# Whether the generator should be tested
+gen_loss = False
+error = False
+
+for epoch in range(n_epochs):
+  
+    # Dataloader returns the batches
+    for real, _ in tqdm(dataloader):
+        cur_batch_size = len(real)
+
+        # Flatten the batch of real images from the dataset
+        real = real.view(cur_batch_size, -1).to(device)
+
+        ### Update discriminator ###
+        # Zero out the gradients before backpropagation
+        disc_opt.zero_grad()
+
+        # Calculate discriminator loss
+        disc_loss = get_disc_loss(gen, disc, criterion, real, cur_batch_size, z_dim, device)
+
+        # Update gradients
+        disc_loss.backward(retain_graph=True)
+
+        # Update optimizer
+        disc_opt.step()
+
+        # For testing purposes, to keep track of the generator weights
+        if test_generator:
+            old_generator_weights = gen.gen[0][0].weight.detach().clone()
+
+
+        ### Update generator ###
+        gen_opt.zero_grad()
+        gen_loss = get_gen_loss(gen, disc, criterion, cur_batch_size, z_dim, device)
+        gen_loss.backward(retain_graph=True)
+        # backward = backpropagation
+        gen_opt.step()
+        # update the Ws and biases
+
+
+        # For testing purposes, to check if the generator weights is being updated
+        if test_generator:
+            try:
+                assert lr > 0.0000002 or (gen.gen[0][0].weight.grad.abs().max() < 0.0005 and epoch == 0)
+                assert torch.any(gen.gen[0][0].weight.detach().clone() != old_generator_weights)
+            except:
+                error = True
+                print("Runtime tests have failed")
+
+        # Keep track of the average discriminator loss
+        mean_discriminator_loss += disc_loss.item() / display_step
+
+        # Keep track of the average generator loss
+        mean_generator_loss += gen_loss.item() / display_step
+
+        ### Visualization code ###
+        if cur_step % display_step == 0 and cur_step > 0:
+            print(f"Epoch {epoch}, step {cur_step}: Generator loss: {mean_generator_loss}, discriminator loss: {mean_discriminator_loss}")
+            fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+            fake = gen(fake_noise)
+            show_tensor_images(fake)
+            show_tensor_images(real)
+            mean_generator_loss = 0
+            mean_discriminator_loss = 0
+        cur_step += 1
